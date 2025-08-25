d0d5648b51b1d39ecbe889975daf82bcee7376d8
Kusoro Adeolu <kusoroadeolu@gmail.com>
2025-08-25 21:28:42 +0100

    Added jpa query

--- src/main/java/com/victor/VibeMatch/user/UserRepository.java
+++ src/main/java/com/victor/VibeMatch/user/UserRepository.java
@@ -3,6 +3,8 @@ package com.victor.VibeMatch.user;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -20,7 +22,8 @@ public interface UserRepository extends JpaRepository<User, UUID> {
     List<User> findByLastSyncedAtBefore(LocalDateTime now);
 
     @Lock(LockModeType.PESSIMISTIC_WRITE)
-    Optional<User> findUserBySpotifyIdWithLock(String spotifyId);
+    @Query("SELECT u FROM User u WHERE u.spotifyId = :id")
+    Optional<User> findUserBySpotifyIdWithLock(@Param("id") String spotifyId);
 
     List<User> findByIsPublicTrue();
 }
